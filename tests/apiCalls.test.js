32b63f570c1f14b99d0124f6a275fe9c08098670
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-27 13:52:08 +0100

    Fix test typo

--- tests/apiCalls.test.js
+++ tests/apiCalls.test.js
@@ -1,5 +1,5 @@
 const apiCalls = require("../lib/apiCalls.js");
 
 test("Returns at max 30 items", () => {
-  expect(apiCalls.searchByUsername("query").length).toBeLessThanOrEqualTo(30);
+  expect(apiCalls.searchByUsername("query").length).toBeLessThanOrEqual(30);
 });
