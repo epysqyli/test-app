27497d259bea359cab9af471a534f4005be1a881
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-27 12:34:49 +0100

    Add SearchForm component, install sass

---
+++ components/SearchForm.jsx
@@ -0,0 +1,24 @@
+import { Search } from "react-feather";
+
+const SearchForm = () => {
+  return (
+    <div className="flex group rounded-md shadow-md">
+      <input
+        type="text"
+        name="tmp"
+        id="tmp"
+        className="border-none bg-white w-5/6 rounded-tl-lg rounded-bl-lg focus:ring-0 group-hover:shadow-md transition py-2"
+        required
+      />
+
+      <button
+        type="submit"
+        className="w-1/6 text-center bg-white border-l rounded-tr-lg rounded-br-lg hover:bg-gray-200 group-hover:shadow-md transition active:bg-gray-400 active:text-white"
+      >
+        <Search size={20} className="mx-auto" color="gray" />
+      </button>
+    </div>
+  );
+};
+
+export default SearchForm;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import SearchForm from "../components/SearchForm";
 
 const Home = () => {
   return (
@@ -9,11 +10,17 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="text-4xl font-medium text-center mt-5">
-        Github users search
+      <div className="w-4/5 mx-auto ">
+        <div className="text-4xl font-medium text-center mt-5">
+          Github users search
+        </div>
+
+        <div className="mt-10">
+          <SearchForm />
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Home;
