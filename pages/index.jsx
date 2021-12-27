7d044dd15bfa95f9bf3865f5c2425030d8043956
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-27 12:18:35 +0100

    Remove feather-icons and install react-feather

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,19 @@
 import Head from "next/head";
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <Head>
-        <title>Home page</title>
+        <title>Github Users Search</title>
         <meta name="homepage" content="Github users custom search" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <div className="text-4xl font-medium text-center mt-5">
+        Github users search
+      </div>
     </div>
   );
 }
+
+export default Home;
