6b1d776d5ec746d63ec6ea1eff55ce9f2aee9d35
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-27 10:35:57 +0100

    Setup tailwind

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home page</title>
+        <meta name="homepage" content="Github users custom search" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
