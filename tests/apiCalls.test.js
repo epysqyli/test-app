718147ac9c40f4248cf72ab1b1e408c7dabf684a
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-28 00:05:57 +0100

    Add memory-cache and cacheCall in lib/apiCalls

--- tests/apiCalls.test.js
+++ tests/apiCalls.test.js
@@ -1,6 +1,6 @@
 const apiCalls = require("../lib/apiCalls.js");
 
-xtest("Returns json resp with at max 30 user items", async () => {
+test("Returns json resp with at max 30 user items", async () => {
   const resp = await apiCalls.searchByUsername();
 
   expect(resp.data.items.length).toBeLessThanOrEqual(30);
