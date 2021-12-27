33fae840a99359b2c61a721c68aa571e5828d96c
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-27 16:46:37 +0100

    Add conditional link for Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,20 +1,35 @@
 import { ArrowUpRight } from "react-feather";
 import Link from "next/link";
+import Router, { useRouter } from "next/router";
 
 const Footer = () => {
-  return (
-    <Link href="/history" passHref={true}>
-      <div className="flex items-center justify-center gap-x-5 p-10 group cursor-pointer border-t hover:bg-gray-200 active:bg-gray-300">
-        <div className="text-gray-700">
-          Check search history and latest results
-        </div>
-        <ArrowUpRight
-          color="gray"
-          className="group-hover:scale-125 transition-transform"
-        />
+  const baseFooter = (text) => (
+    <div className="flex items-center justify-center gap-x-5 p-10 group cursor-pointer border-t hover:bg-gray-200 active:bg-gray-300">
+      <div className="text-gray-700">
+        {text}
       </div>
-    </Link>
+      <ArrowUpRight
+        color="gray"
+        className="group-hover:scale-125 transition-transform"
+      />
+    </div>
   );
+
+  const router = useRouter();
+
+  if (router.pathname == "/")
+    return (
+      <Link href="/history" passHref={true}>
+        {baseFooter("Check history and latest results")}
+      </Link>
+    );
+
+  if (router.pathname == "/history")
+    return (
+      <Link href="/" passHref={true}>
+        {baseFooter("Back to search")}
+      </Link>
+    );
 };
 
 export default Footer;
