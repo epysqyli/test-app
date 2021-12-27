b146408dcd1c9705a0881ca1c1a1d5ba5c2dc9c2
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-27 14:41:40 +0100

    Add api call test for user search

--- tests/apiCalls.test.js
+++ tests/apiCalls.test.js
@@ -1,5 +1,6 @@
 const apiCalls = require("../lib/apiCalls.js");
 
-test("Returns at max 30 items", () => {
-  expect(apiCalls.searchByUsername("query").length).toBeLessThanOrEqual(30);
+test("Returns json resp with at max 30 user items", async () => {
+  const resp = await apiCalls.searchByUsername();
+  expect(resp.data.items.length).toBeLessThanOrEqual(30);
 });
