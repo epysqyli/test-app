bcccb2c85cc1da62c773a81a5dd9d89a8c36e8fe
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-12-28 10:46:09 +0100

    Add arrow icon to keyword component

--- components/Keyword.jsx
+++ components/Keyword.jsx
@@ -1,5 +1,12 @@
+import { ArrowUpRight } from "react-feather";
+
 const Keyword = ({ historyItem }) => {
-  return <div className="px-2 py-3 text-center">{historyItem}</div>;
+  return (
+    <div className="flex items-center justify-around group cursor-pointer group">
+      <div className="px-2 py-3 text-center">{historyItem}</div>
+      <ArrowUpRight size={18} color="gray" className="group-hover:scale-125" />
+    </div>
+  );
 };
 
 export default Keyword;
